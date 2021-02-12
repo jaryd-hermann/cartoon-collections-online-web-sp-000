8b53c040268ccdd7aa4cf7d93ef123bb33863372
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-12 00:10:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(array)
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  return true if array.include?{|word| word.length > 4}
+  false
 end
 
 def find_the_cheese# code an argument here
